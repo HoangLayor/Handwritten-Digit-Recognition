861d034111ad836d05347478ad2883d900c9e401
HoangLayor <giahoang481@gmail.com>
2024-10-18 00:32:07 +0700

    updated api

--- app/api.py
+++ app/api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
-from inference import predict
+# from inference import predict
 from PIL import Image
 from torchvision import transforms
 from utils.utils import load_checkpoint
@@ -7,8 +7,19 @@ from utils.preprocess import preprocess_image
 from models.model import Net
 import torch
 import io
+import asyncio
 
 app = FastAPI()
+
+model = Net()
+model, _, _ = load_checkpoint(r'checkpoints\checkpoint_1\best accuracy\mnist_model_best.pth', model, None)
+
+async def predict(img_tensor):
+    with torch.no_grad():
+        output = model(img_tensor.unsqueeze(0))
+        predicted = torch.argmax(output, dim=1)
+
+    return predicted
 
 # API để upload file ảnh
 @app.post("/upload-image/")
@@ -18,9 +29,6 @@ async def upload_image(file: UploadFile = File(...)):
 
     # Mở ảnh bằng Pillow
     image = Image.open(io.BytesIO(image_data))
-
-    # Bạn có thể xử lý ảnh ở đây, ví dụ lấy kích thước ảnh
-    width, height = image.size
 
     return {"image": image}
 
@@ -35,28 +43,18 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         return {'error': str(e)}
     
-    # Giả sử bạn đã load model MNIST đã train xong
-# Ví dụ, bạn lưu model vào file model_mnist.pth sau khi train
-model = Net()
-model, _, _ = load_checkpoint(r"checkpoints\checkpoint_1\best accuracy\mnist_model_best.pth", model, None)
-# model.eval()
-
-# Chuyển đổi ảnh về dạng mà model yêu cầu
-transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),  # Chuyển ảnh sang grayscale
-    transforms.Resize((28, 28)),  # Resize về 28x28 giống MNIST
-    transforms.ToTensor(),  # Chuyển ảnh sang Tensor
-    transforms.Normalize((0.1307,), (0.3081,))  # Normalize giống như cách bạn đã train
-])
-
+    
 # API để upload file ảnh và nhận diện
 @app.post("/predict_123/")
 async def predict(file: UploadFile = File(...)):
     # Đọc file ảnh
-    image_data = await file.read()
-    image = Image.open(io.BytesIO(image_data))
+    image = Image.open(io.BytesIO(await file.read()))
 
     img_tensor = preprocess_image(image)
-    pred = predict(img_tensor)
-    return {pred}
+    predicted = await predict(img_tensor)
+    # with torch.no_grad():
+    #     output = model(img_tensor.unsqueeze(0))
+    #     predicted = torch.argmax(output, dim=1)
+
+    return {"prediction": predicted.item()}
     
