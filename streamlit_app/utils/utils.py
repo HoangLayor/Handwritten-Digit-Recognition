41c21b8affdc1bd9639b3d763248be078efdcd3d
HoangLayor <giahoang481@gmail.com>
2024-10-18 14:43:55 +0700

    Updated path

--- streamlit_app/utils/utils.py
+++ streamlit_app/utils/utils.py
@@ -15,7 +15,7 @@ def load_checkpoint(path, model, optimizer=None):
         optimizer.load_state_dict(checkpoint['optimizer'])
     return model, optimizer, checkpoint['epoch']
 
-def get_model(model_path=r'src\checkpoints\checkpoint_1\best accuracy\mnist_model_best.pth'):
+def get_model(model_path=r'src/checkpoints/checkpoint_1/best accuracy/mnist_model_best.pth'):
     model = Net()
     model, _, _ = load_checkpoint(model_path, model, None)
     model.eval()
